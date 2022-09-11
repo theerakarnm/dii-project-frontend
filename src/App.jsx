c95fff9a78c0e37cfa0a4f972849dcdb1a085746
Ryoneme2 <worknarair6@gmail.com>
2022-09-11 18:28:38 +0700

    feature : ui feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Feed from "./pages/Feed";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/feed" element={<Feed />} />
     </Routes>
   );
 }
---
+++ src/pages/Feed.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Container from "../layouts/Container";
+import Post from "../components/Posts";
+import mockData from "../mocks/postData";
+import PostLayout from "../layouts/PostLayout";
+
+const Feed = () => {
+  // TODO : check login
+
+  return (
+    <>
+      <Navbar />
+      <Container>
+        <div className="flex flex-col gap-5 justify-center items-center">
+          {mockData.map((item) => {
+            return (
+              <PostLayout>
+                <Post postData={item} />
+              </PostLayout>
+            );
+          })}
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Feed;
