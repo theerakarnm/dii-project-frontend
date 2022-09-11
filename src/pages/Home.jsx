ac0079b845759f6b9d24abf50e0194564e929878
Ryoneme2 <worknarair6@gmail.com>
2022-09-11 15:12:41 +0700

    example navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Navbar from "../components/Navbar";
+
 const Home = () => {
   return (
     <>
+      <Navbar />
       <div>
         <h1 className="text-red-800">asfsad</h1>
       </div>
