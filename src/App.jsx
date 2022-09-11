f6327dbd02d035de4a585ab208fc3c353e7db38e
Nuthipong <nuthipong_p@cmu.ac.th>
2022-09-11 16:29:20 +0700

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
--- src/pages/Login.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Login = () => {
-  return <div>Login alsdfh</div>;
-};
-
-export default Login;
