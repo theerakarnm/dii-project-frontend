29435cbf2ed859ebd3c04ddb3839b7f5e20680d2
Nuthipong <nuthipong_p@cmu.ac.th>
2022-09-12 13:25:21 +0700

    staged login

--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -1,91 +1,9 @@
-import React from "react";
-import { Input, useInput, Grid } from "@nextui-org/react";
-
-export default function LoginForm() {
-  const { value, reset, bindings } = useInput("");
-
-  const validateEmail = (value) => {
-    return value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
-  };
-
-  const helper = React.useMemo(() => {
-    if (!value)
-      return {
-        text: "",
-        color: "",
-      };
-    const isValid = validateEmail(value);
-    return {
-      text: isValid ? "Correct email" : "Enter a valid email",
-      color: isValid ? "success" : "error",
-    };
-  }, [value]);
+const LoginForm = () => {
   return (
-    <Grid.Container gap={4}>
-      <Grid>
-        <Input
-          {...bindings}
-          clearable
-          shadow={false}
-          onClearClick={reset}
-          status={helper.color}
-          color={helper.color}
-          helperColor={helper.color}
-          helperText={helper.text}
-          type="email"
-          label="Email"
-          placeholder="With regex validation"
-        />
-      </Grid>
-      <Grid>
-        <Input
-          clearable
-          helperText="Please enter your name"
-          label="Name"
-          placeholder="Enter your name"
-        />
-      </Grid>
-      <Grid>
-        <Input
-          clearable
-          color="error"
-          helperText="Required"
-          label="Error"
-          placeholder="Enter something"
-        />
-      </Grid>
-      <Grid>
-        <Input
-          clearable
-          color="success"
-          initialValue="getnextui"
-          helperText="Excelent username"
-          type="test"
-          label="Username"
-          placeholder="Enter your username"
-        />
-      </Grid>
-      <Grid>
-        <Input
-          clearable
-          color="warning"
-          helperText="Insecure password"
-          type="password"
-          label="Password"
-          placeholder="Enter your password"
-        />
-      </Grid>
-      <Grid>
-        <Input.Password
-          clearable
-          color="warning"
-          initialValue="123"
-          helperText="Insecure password"
-          type="password"
-          label="Password"
-          placeholder="Enter your password with eye"
-        />
-      </Grid>
-    </Grid.Container>
+    <div className="h-screen bg-[f9fafe] flex flex-col justify-center items-center">
+      <div className="bg-white w-96 p-8 rounded-lg shadow-xl">Login</div>
+    </div>
   );
-}
+};
+
+export default LoginForm;
