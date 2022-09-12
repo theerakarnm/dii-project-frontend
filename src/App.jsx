e9142054bdab20b71bc00ca9bf675c340fcf6a00
FourthTs <fourth4884@gmail.com>
2022-09-12 13:16:06 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Feed from "./pages/Feed";
-import SignUP from "./pages/Regis";
+import Regis from "./pages/Regis";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/feed" element={<Feed />} />
-      <Route path="/signUp" element={<SignUP />} />
+      <Route path="/signUp" element={<Regis />} />
     </Routes>
   );
 }
