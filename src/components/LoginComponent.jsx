7b7f9c8723d58b70dd21458278553becebaca338
Ryoneme2 <worknarair6@gmail.com>
2022-09-15 10:37:28 +0700

    setError login page

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -8,6 +8,16 @@ const LoginComponent = () => {
     username: "",
     password: "",
   });
+  const [isError, setIsError] = useState({
+    username: {
+      error: false,
+      msg: "",
+    },
+    password: {
+      error: false,
+      msg: "",
+    },
+  });
 
   function onSubmit(event) {
     event.preventDefault();
@@ -34,59 +44,71 @@ const LoginComponent = () => {
             />
           </div>
 
-          <form onSubmit={onSubmit}></form>
-          <div className="w-full p-2  rounded ">
-            <div className="flex flex-col justify-center items-center">
-              <h1 className="text-4xl font-bold">Sign in to your account</h1>
-            </div>
-          </div>
-          <div className="bg-white max-w-xl  w-[38rem] h-[25rem] flex flex-col  p-8 rounded-lg shadow-xl">
-            <div className="w-full p-2  rounded my-5">
-              <div className="flex flex-col justify-center items-center my-10`">
-                <input
-                  className="w-full p-3 border rounded text-[1.25rem]"
-                  type="text"
-                  name="username"
-                  id="username"
-                  onChange={onChangeHandle}
-                  value={valueInput.username}
-                  placeholder="Username"
-                />
-              </div>
-
-              <div className="flex flex-col justify-center items-center my-12">
-                <input
-                  className="w-full p-3 border rounded text-[1.25rem]"
-                  type="password"
-                  name="password"
-                  id="password"
-                  value={valueInput.password}
-                  placeholder="Password"
-                  onChange={onChangeHandle}
-                />
-              </div>
-
-              <div className="flex flex-col justify-center items-center mt-10">
-                <button
-                  className="w-full p-3 transition-all bg-purple-500 hover:bg-purple-600 text-white rounded text-[1.25rem]"
-                  name="bt_submit"
-                  id="bt_submit"
-                  type="submit"
-                >
-                  Sign in
-                </button>
-              </div>
-
-              <div className="flex flex-col justify-center items-center my-2">
-                <p>
-                  Don't have an account ?
-                  <a className=" text-purple-600 hover:text-purple-700 hover:font-bold">
-                    Sign up
-                  </a>
-                </p>
+          <form onSubmit={onSubmit}>
+            <div className="w-full p-2 rounded ">
+              <div className="flex flex-col justify-center items-center">
+                <h1 className="text-4xl font-bold">Sign in to your account</h1>
               </div>
             </div>
-          </div>
+            <div className="bg-white max-w-xl  w-[38rem] h-[25rem] flex flex-col  p-8 rounded-lg shadow-xl">
+              <div className="w-full p-2  rounded my-5">
+                <div className="flex flex-col justify-center items-center my-10`">
+                  <input
+                    className={`w-full p-3 border rounded text-[1.25rem] ${
+                      isError.username.error ? "ring-2 ring-red-400" : ""
+                    }`}
+                    type="text"
+                    name="username"
+                    id="username"
+                    onChange={onChangeHandle}
+                    value={valueInput.username}
+                    placeholder="Username"
+                  />
+                  {isError.username.error ? (
+                    <div className="flex justify-start w-full">
+                      <small className="text-left text-red-400">
+                        {isError.username.msg}
+                      </small>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+
+                <div className="flex flex-col justify-center items-center my-12">
+                  <input
+                    className="w-full p-3 border rounded text-[1.25rem]"
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={valueInput.password}
+                    placeholder="Password"
+                    onChange={onChangeHandle}
+                  />
+                </div>
+
+                <div className="flex flex-col justify-center items-center mt-10">
+                  <button
+                    className="w-full p-3 transition-all bg-purple-500 hover:bg-purple-600 text-white rounded text-[1.25rem]"
+                    name="bt_submit"
+                    id="bt_submit"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                </div>
+
+                <div className="flex flex-col justify-center items-center my-2">
+                  <p>
+                    Don't have an account ?
+                    <a className=" text-purple-600 hover:text-purple-700 hover:font-bold">
+                      Sign up
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </>
