f2da41f17fa6ea685c0533a3a32624ce6d0a0da6
FourthTs <fourth4884@gmail.com>
2022-09-17 16:39:17 +0700

    card in home page

--- src/components/Dairy.jsx
+++ src/components/Dairy.jsx
@@ -1,9 +1,8 @@
+/* This example requires Tailwind CSS v2.0+ */
+
 import React from "react";
 
 const Diary = () => {
-return(
-  <></>
-)
-}
+
 
 export default Diary;
--- src/components/Homeform.jsx
+++ src/components/Homeform.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Avatar, Grid, Textarea, User, Text } from "@nextui-org/react";
+import { Avatar, Textarea, Text, Card, Modal, Image } from "@nextui-org/react";
+import { m } from "framer-motion";
+
+
 
 
 
 const Homeform = () => {
+
+  const [visible, setVisible] = React.useState(false);
+  const handler = () => setVisible(true);
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center ">
         <div className="max-w-5xl w-full h-full flex flex-col m-auto">
-          <div className=" w-full h-auto  grid grid-cols-3 md:gap-4 gap-0 items-center my-3 p-2">
+          <div className=" w-full  grid grid-cols-3 md:gap-4 gap-0 items-center my-3 p-2 ">
 
             <div className="md:p-2 p-1">
               <Text
@@ -19,29 +30,28 @@ const Homeform = () => {
                   textGradient: "45deg, $purple600 -20%, $pink600 100%",
                 }}
               >
-                Nuthipong Pinyai
+                <p><span className="">Nuthipong</span> <span className="">Pinyai</span> </p>
               </Text>
               <Text
                 h1
-                className="md:text-[1rem] text-[0.8rem]"
+                className="md:text-[1rem] text-[0.8rem] "
                 css={{
                   textGradient: "45deg, $purple600 -20%, $pink600 100%",
                 }}
-                
+
                 weight="bold"
               >
                 click_ntp@testmail.com
               </Text>
             </div>
 
-            <div className=" flex justify-center items-center" >
+            <div className=" flex justify-center items-center ">
               <div className="md:h-[12rem] md:w-[12rem] w-[7rem] h-[7rem]">
                 <Avatar
                   className="w-full h-full"
                   src="/Logowithoutfont.png"
                   color="secondary"
                   bordered
-                  zoomed
                 />
               </div>
             </div>
@@ -65,9 +75,74 @@ const Homeform = () => {
               </div>
             </div>
           </div>
-          <div className="w-full border-t border-purple-300 flex justify-center items-center p-3 mt-3">
-            <p>5555</p>
+
+          <div className="w-full border-t border-purple-300 flex justify-center"></div>
+
+          <div className="w-full h-full  grid gap-4 md:grid-cols-3 grid-cols-2 px-5">
+
+            <div className="  flex items-center">
+              <Card isPressable onClick={handler}
+                css={{ w: "100%", h: "85%", }}
+              >
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+
+                </Card.Header>
+                <Card.Body css={{ p: 0 }}>
+                  <Card.Image
+                    src="https://earena.com/media/team/logo/500x500.jpeg"
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    alt="Card example background"
+                  />
+                </Card.Body>
+              </Card>
+
+              <Modal noPadding open={visible} onClose={closeHandler}
+              >
+                <Modal.Body>
+                      <div className="w-[50rem] h-[50rem] bg-slate-500">
+
+                      </div>
+                </Modal.Body>
+              </Modal>
+
+            </div>
+            
+
+            <div className="  flex items-center">
+              <Card isPressable onClick={handler}
+                css={{ w: "100%", h: "85%", }}
+              >
+                <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+
+                </Card.Header>
+                <Card.Body css={{ p: 0 }}>
+                  <Card.Image
+                    src="https://i2.wp.com/dynamicwork.net/wp/wp-content/uploads/2013/10/500x500.gif?ssl=1"
+                    width="100%"
+                    height="100%"
+                    objectFit="cover"
+                    alt="Card example background"
+                  />
+                </Card.Body>
+              </Card>
+
+              <Modal noPadding open={visible} onClose={closeHandler}
+              >
+                <Modal.Body>
+                      <div className="w-[50rem] h-[50rem] bg-slate-500">
+
+                      </div>
+                </Modal.Body>
+              </Modal>
+
+            </div>
+            
+
+
           </div>
+
         </div>
       </div>
     </>
