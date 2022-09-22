236b7ca3b12fc086dd2e7d5256e425c0e6f0f7c5
Nuthipong <nuthipong_p@cmu.ac.th>
2022-09-22 15:03:54 +0700

    Diary complete wait for backend

--- src/components/DairyComponent.jsx
+++ src/components/DairyComponent.jsx
@@ -6,77 +6,33 @@ const DiaryComponent = () => {
 
   return (
     <>
-      <div className="flex justify-center bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-56 max-w-md md:max-w-2xl">
-        <div className=" flex items-start px-4 py-6 w-full">
-          <img
-            className="w-12 h-12 rounded-full object-cover mr-4 shadow"
-            src={`${cookie.imageUrl}`}
-            alt="avatar"
-          />
-          <div className="">
-            <div className="flex items-center justify-between">
-              <h2 className="text-lg font-semibold text-gray-900 -mt-1">
-                Brad Adams{" "}
-              </h2>
-              <small className="text-sm text-gray-700">22h ago</small>
+      <div className="flex justify-center bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-56 max-w-3xl md:max-w-[55rem] p-3">
+        <div className="flex flex-row items-start px-4 py-6 w-full">
+          <div className="mr-2">
+            <img
+              className="w-12 h-12 rounded-full object-cover mr-4 shadow"
+              src={`${cookie.imageUrl}`}
+              alt="avatar"
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="flex justify-between">
+              <div>
+                <p class="text-xl font-semibold mb-2">
+                  {`${cookie.firstName} ${cookie.lastName}`}
+                </p>
+              </div>
+              <div>
+                <p className="text-sm mt-1">22h ago</p>
+              </div>
             </div>
-            <p className="text-gray-700">Joined 12 SEP 2012. </p>
-            <p className="mt-3 text-gray-700 text-sm">
-              <div className="w-96 h-96 border-2 border-purple-400 ">test</div>
-            </p>
-            <div className="mt-4 flex items-center">
-              <div className="flex  text-gray-700 text-sm mr-3">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                  />
-                </svg>
-                <span>12</span>
-              </div>
-              <div className="flex  text-gray-700 text-sm mr-8">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                  />
-                </svg>
-                <span>8</span>
-              </div>
-              <div className="flex text-gray-700 text-sm mr-4">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                  />
-                </svg>
-                <span>share</span>
-              </div>
+            <div className="w-[45rem] h-[38rem] border border-purple-300 ">
+              img
             </div>
           </div>
         </div>
       </div>
+      ;
     </>
   );
 };
