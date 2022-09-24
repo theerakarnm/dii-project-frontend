ce7c1f7ef37f4034931dc93b71aedca4721f00d2
Nuthipong <nuthipong_p@cmu.ac.th>
2022-09-24 13:11:00 +0700

    insert whiteboard to Dairy component

--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -1,14 +1,14 @@
-import SignatureCanvas from 'react-signature-canvas';
+import SignatureCanvas from "react-signature-canvas";
 
-import { useState, useMemo, useRef } from 'react';
-import DropdownCom from './Utils/Dropdown';
+import { useState, useMemo, useRef } from "react";
+import DropdownCom from "./Utils/Dropdown";
 
 const Drawing = ({ css }) => {
-  const [color, setColor] = useState(new Set(['black']));
+  const [color, setColor] = useState(new Set(["black"]));
   const boardRef = useRef(null);
 
   const selectedColor = useMemo(
-    () => Array.from(color).join(', ').replaceAll('_', ' '),
+    () => Array.from(color).join(", ").replaceAll("_", " "),
     [color]
   );
 
@@ -21,21 +21,21 @@ const Drawing = ({ css }) => {
   return (
     <>
       <div>
-        <div className='flex mb-2'>
+        <div className="flex mb-2">
           <DropdownCom setColor={setColor} />
           <button
-            className='bg-red-400 hover:bg-red-500 text-white px-3 py-1 rounded-xl ml-2'
+            className="bg-red-400 hover:bg-red-500 text-white px-3 py-1 rounded-xl ml-2"
             onClick={onClearBoard}
           >
             clear
           </button>
         </div>
-        <div className={`bg-white ${{ css }}`}>
+        <div className={`bg-slate-50 ${{ css }}`}>
           <SignatureCanvas
             ref={boardRef}
             throttle={5}
             penColor={selectedColor.toLowerCase()}
-            canvasProps={{ width: 500, height: 400, className: 'sigCanvas' }}
+            canvasProps={{ width: 500, height: 400, className: "sigCanvas" }}
           />
         </div>
       </div>
